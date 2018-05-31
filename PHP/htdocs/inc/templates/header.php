1211c5233b1e232397986ed62dff0bc60ca8ab18
Josedn <joserafael161@hotmail.com>
2018-05-30 19:49:42 -0500

    Agregando PHP

---
+++ PHP/htdocs/inc/templates/header.php
@@ -0,0 +1,11 @@
+<?php if (!defined("UBER")) exit; ?><header>
+  <section class="header_overlay">
+    <section class="little_container">
+
+      <section id="logo" class="boy">
+        <a href="/" >habboinn</a>
+      </section>
+
+    </section>
+  </section>
+</header>
